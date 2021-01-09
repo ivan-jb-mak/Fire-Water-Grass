45dab6a04e418404d7b0f6a7633768f585b6478b
thingling <ivanjbmak@gmail.com>
2021-01-08 22:57:02 -0500

    Completed pokemon Fire-Water-Grass project

--- app.js
+++ app.js
@@ -1,63 +1,114 @@
-// function to randomly return fire, water or grass
-function computerPlay() {
-    const FIRE_WATER_GRASS = ["FIRE", "WATER", "GRASS"]; // initianting array with 3 possible results
-    return FIRE_WATER_GRASS[Math.floor(Math.random() * FIRE_WATER_GRASS.length)]; // return a random index of the array
-}
+const game = ()=> {
+    // creating variables for the player and computer scores
+    let pScore = 0; // computer
+    let cScore = 0; // player
 
-// function to play 1 round of fire, water or grass
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase(); // making user input case insensitive
+    // startGame function to change the screen content to start playing game
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
 
-    // constant variables to store output string for lose or win
-    const WIN = "You WIN! " + playerSelection + " beats " + computerSelection;
-    const LOSE = "You LOSE! " + computerSelection + " beats " + playerSelection;
-    const TIE = "It's a tie! You both chose " + playerSelection; 
+        // when user clicks the play button, fades into the next "screen"
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
 
-    // if-else statements to determine winner based off of player and computer selection
-    if (playerSelection === computerSelection) {
-        return TIE  
+    //Play match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+
+        // array for computer options
+        const computerOptions = ["fire", "water", "grass"];
+
+        options.forEach(option => {
+            option.addEventListener("click", function () {
+                // computer Choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerSelection = computerOptions[computerNumber];
+                
+                compareHands(this.textContent, computerSelection);
+
+                // updating the images depending on the player and computer choices 
+                playerHand.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerSelection}.png`;
+            });
+        });
+    };
+
+    // to update the score
+    function updateScore() {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+        return;
     }
-    else if (playerSelection === "FIRE") {
-        if (computerSelection === "WATER") {
-            return LOSE; // retuns losing statement with details on choices
+
+    function compareHands(playerSelection, computerSelection) {
+        playerSelection = playerSelection.toLowerCase(); // making user input case insensitive
+    
+        // constant variables to store output string for lose or win
+        const WIN = "You WIN! " + playerSelection + " beats " + computerSelection;
+        const LOSE = "You LOSE! " + computerSelection + " beats " + playerSelection;
+        const TIE = "It's a tie! You both chose " + playerSelection; 
+    
+        const winner = document.querySelector('.winner');
+        // if-else statements to determine winner based off of player and computer selection
+        // checking for a tie
+        if (playerSelection === computerSelection) {
+            winner.textContent = TIE;
+            return;
         }
-        else {
-            return WIN; // retuns winning statement with details on choices
+        else if (playerSelection === "fire") {
+            if (computerSelection === "water") {
+                winner.textContent = LOSE;
+                cScore++; // increasing the computer score by 1
+                updateScore(); // calling the updateScore function to change on html
+                return;
+            }else {
+                winner.textContent = WIN;
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+        else if (playerSelection === "water") {
+            if (computerSelection === "grass") {
+                winner.textContent = LOSE;
+                cScore++;
+                updateScore();
+                return;
+            }else {
+                winner.textContent = WIN;
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+        else if (playerSelection === "grass") {
+            if (computerSelection === "fire") {
+                winner.textContent = LOSE;
+                cScore++;
+                updateScore();
+                return;
+            }else {
+                winner.textContent = WIN;
+                pScore++;
+                updateScore();
+                return;
+            }
         }
     }
-    else if (playerSelection === "WATER") {
-        if (computerSelection === "GRASS") {
-            return LOSE;
-        }
-        else {
-            return WIN;
-        }
-    }
-    else if (playerSelection === "GRASS") {
-        if (computerSelection === "FIRE") {
-            return LOSE;
-        }
-        else {
-            return WIN;
-        }
-    }
-}
 
-// function to play the game of rock, paper, scissors with user input x amount of times
-function game(numOfGames) {
+    //all all the inner funcitons
+    startGame();
+    playMatch();
+};
 
-    // loop to iterate through the game, asking for player input, and 
-    for (let gameIndex = 0; gameIndex < numOfGames; gameIndex++) {
-
-        let playerSelection = prompt("Rock, Paper, or Scissors?: "); // asking for player input for game
-        const computerSelection = computerPlay(); // generating a random answer from computer
-
-        console.log("You selected: " + playerSelection.toUpperCase());
-        console.log("The computer selected: " + computerSelection); 
-
-        // calling function to play 1 round, and output results
-        console.log(playRound(playerSelection,computerSelection)); 
-    }
-}
-
+game();
 
